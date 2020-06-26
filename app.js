ccc02e3b1a9024a2b1162cd5e96743e20d8ecae8
AbdulNaveed951 <64982365+AbdulNaveed951@users.noreply.github.com>
2020-06-26 20:01:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+{/* <span id="clear" onclick="document.getElementById('answer').value =''">C</span>
+ 
+<span onclick="document.getElementById('answer').value+='%'">%</span>   
+<span onclick="document.getElementById('answer').value+='.'">.</span>
+<span onclick="document.getElementById('answer').value+='*'">*</span>  
+<span onclick="document.getElementById('answer').value+=7">7</span>    
+<span onclick="document.getElementById('answer').value+=8">8</span>
+<span onclick="document.getElementById('answer').value+=9">9</span>
+<span onclick="document.getElementById('answer').value+='-'">-</span>
+<span onclick="document.getElementById('answer').value+=4">4</span>
+<span onclick="document.getElementById('answer').value+=5">5</span>
+<span onclick="document.getElementById('answer').value+=6">6</span>
+<span id="add" onclick="document.getElementById('answer').value+='+'">+</span>
+<span onclick="document.getElementById('answer').value+=1">1</span>
+<span onclick="document.getElementById('answer').value+=2">2</span>
+<span onclick="document.getElementById('answer').value+=3">3</span>
+<span onclick="document.getElementById('answer').value+='/'">/</span>
+<span onclick="document.getElementById('answer').value+=0">0</span>
+<span onclick="document.getElementById('answer').value+='('">(</span>
+<span onclick="document.getElementById('answer').value+=')'">)</span>
+<span id="equal" onclick="document.getElementById('answer').value=eval(document.getElementById('answer').value)">=</span> */}
